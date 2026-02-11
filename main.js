dfb031071ffffe46c92520796778f5d014c2b108
ajs <140010746+aboodJS@users.noreply.github.com>
2026-02-11 18:06:50 +0300

    made some changes

--- main.js
+++ main.js
@@ -1,31 +1,55 @@
 import express, { json } from "express";
 import { hash as _hash } from "bcrypt";
+import { Pool } from "pg";
 import "dotenv/config";
 import cors from "cors";
 import { neon } from "@neondatabase/serverless";
-const port = process.env.PORT;
-const sql = neon(process.env.DATABASE_URL);
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+const pools = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
+
 app.post("/signup", async (req, res) => {
-  console.log(req.body);
-  _hash(req.body.password, 10, function (err, hash) {
+  _hash(req.body.password, 10, async (err, hash) => {
+    const client = await pools.connect();
     try {
-      res.send({ username: req.body.username, "hashed password": hash });
+      const result = await client.query(
+        `INSERT INTO users VALUES ('${req.body.username}', '${req.body.email}', '${hash}')`,
+      );
+      res.send({ msg: "request done" });
     } catch (err) {
       res.send({ error: `encountered error: ${err}` });
+    } finally {
+      client.release();
     }
   });
 });
 
 app.get("/users", async (req, res) => {
-  const users = await sql`SELECT * FROM users`;
-  res.send(users);
+  const client = await pools.connect();
+  try {
+    const result = await client.query("SELECT * FROM users");
+    res.json(result.rows);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.release();
+  }
 });
 
-app.listen(port, () => {
-  console.log(`app listening on http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log(`app listening on http://localhost:3000`);
 });
